7499379ebd643bb7807deac7faab6eab9f6efdd7
unknown <pacersuraj@gmail.com>
2026-08-05 10:52:16 +0530

    Use .env for MongoDB Atlas connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/todoDB");
-        console.log("MongoDB Connected");
+        console.log(process.env.MONGO_URI);
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MongoDB Atlas Connected");
     } catch (error) {
         console.log(error);
     }
